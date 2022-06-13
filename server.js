383e2b6f96933667450c066d7eaf424bffa5a687
EdgarMihranyan <erichok1000@gmail.com>
2022-06-13 17:50:56 +0400

    First Commit

--- server.js
+++ server.js
@@ -1,126 +1,164 @@
-"use strict"
-const readlineSync = require("readline-sync");
-const quest = {
-  1(){
-    console.log(`\nHarc N 10.\n Inch ardyunq cuyc kta \`isNaN(NaN) === !isFinite(NaN) \` console - um ` + "\n");
-    return 'true'
-  },
-  2(){
-    console.log(`\nHarc N 9.\n Inch ardyunq cuyc kta \`1 + 2 + 3 + "4" \` console - um ` + "\n");
-    return '64'
-  },
-  3(){
-    console.log(`\nHarc N 8.\n Inch ardyunq cuyc kta \`parseInt('4 + 2') \` console - um ` + "\n");
-    return '4'
-  },
-  4(){
-    console.log(`\nHarc N 7.\n Inch ardyunq cuyc kta \` let a = 0
-    a &&= 2 \` console - um ` + "\n");
-    return '0'
-  },
-  5() {
-    console.log(`\nHarc N 6.\n Inch ardyunq cuyc kta \` let a = 3
-    a **= 2 \` console - um ` + "\n");
-    return '9';
-  },
-  6() {
-    console.log("\nHarc N 5." + "\n Qani data type uni Java Script@ ? " + "\n");
-    return '8';
-  },
-  7() {
-    console.log("\nHarc N 4." + "\n Inch ardyunq cuyc kta ` typeof (NaN) ` Consol - um " + "\n");
-    return "number"
-  },
-  8() {
-    console.log("\nHarc N 3." + "\n Java Scripti data type - neric qanisnen primitive type ? " + "\n");
-    return '7';
-  },
-  9() {
-    console.log("\nHarc N 2." + "\n Inch ardyunq cuyc kta ` null == undefined ` Consol - um " + "\n");
-    return "true"
-  },
-  10() {
-    console.log("\n\nHarc N 1." + "\n Java Script@ qani hat cikl uni ? " + "\n");
-    return "5";
-  },
-}
-class User {
-  constructor(name, age, workPractice) {
-    this.name = name
-    this.age = age
-    this.workPractice = workPractice
-  }
-}
-const user1 = new User(readlineSync.question("Dzer anun - "), Number(readlineSync.question("Dzer tariq@ - ")), Boolean(Number(readlineSync.question(`Ashxatanqayin pordz uneq?
- Ete ayo sexmeq \` 1 
- Ete voch sexmeq \` 0 `+ "\n"))))
-console.log("Bari galust UGeek company xndrum em spaseq ..."+ "\n");
-let promis = new Promise((resolve, reject) => {
-  let user = user1
-  setTimeout(() => {
-    console.log(`Dzer tvyalnern en \`
-   `, user,"\n")
-  }, 3000)
-  setTimeout(() => {
-    if (readlineSync.question(`${user.name} jan dzer tvyalner@ chisht en ?
-    Ete ayo sexmeq \` 1 
-    Ete voch sexmeq \` 0 `+ "\n")) {
-      resolve(user)
-    } else {
-      reject(new Error(`Dzer tvaylner@ sxal en `+ "\n"))
-    }
-    console.log("Ashxatanqayin pordzi stugum ..."+ "\n");
-    user.testWorkPractice = true;
-  }, 7000)
-})
-  .then(data => {
-    return new Promise((res, rej) => {
-      if (data.workPractice) {
-        setTimeout(() => {
-          console.log(data,`\nAshxatanqayin pordz@ arka e \`
-        harcazruyc@ shutov ksksvi ...`+ "\n");
+import { resolve } from 'path';
+import express from 'express';
+import { writeFile, existsSync, readFile } from './utils/fs-promise';
 
-          res(data)
-        }, 8000)
-      } else {
-        setTimeout(() => {
-          console.log(data);
-          console.log(` Harcazruyc chi linelu aranc ashxatanqayin pordzi !`+ "\n");
-        }, 5000)
-        data.interviewResult = false
-        rej(new Error("Ashxatanqayin pordz@ arka che "+ "\n"))
-      }
-    })
-  })
-  .then(data => {
-    return new Promise((res, rej) => {
-      setTimeout(() => console.log("Harcazruyc@ sksvac e"+ "\n"), 6000)
-      let countQuestions = 10
-      let result = 0;
-      setTimeout(() => {
-        while (countQuestions--) {
-          result = quest[countQuestions + 1]() == readlineSync.question(`Dzer patasxane - `) ? result + 1 : result;
-        }
-        console.log(`${data.name} jan duq chisht eq patasxane ${result} harcin 10 ic`+ "\n");
-        if (result >= 8) {
-          data.interviewResult = true
-          res(data)
-        } else {
-          data.interviewResult = false
-          rej(new Error(`${data.name} jan duq cheq ancel harcazruyc@ `+ "\n"))
-        }
-      }, 9000)
-    })
-  }).then(data => {
-    data.worksAt = "UGeeK Company"
-    setTimeout(() => console.log(`Shnorhavorum em ${data.name} duq ancel eq harcazruyc@ ev @ndunvac eq ashxatanqi\n`,data), 3000)
-  })
-  .catch(err => {
-    user1.worksAt = null
-    console.log(user1);
-    setTimeout(() => console.log(err.message + "Hajoxutyun !"), 5000)
-  })
+const checkNames = async (name) => {
+     if (name[0] === name[0].toLowerCase() || name.length < 4) {
+          throw new Error('Name error');
+     }
+     // eslint-disable-next-line no-plusplus
+     for (let i = 0; i < name.length; i++) {
+          if (!(name[i].toUpperCase() >= 'A' && name[i].toUpperCase() <= 'Z')) {
+               throw new Error('Name error');
+          }
+     }
+     return name;
+};
+const checkProperty = async (user) => {
+     const typeUser = {
+          username: true,
+          fNname: true,
+          lName: true,
+          age: true,
+          password: true,
+     };
+     const result = Object.keys(typeUser).join('') === Object.keys(user).join('');
+     if (!result) throw new Error('User property\'s not a found');
+};
+const checkAge = async (age) => {
+     if (!(parseFloat(age) && age <= 150 && age > 0)) {
+          throw new Error('Age error');
+     }
+     return age;
+};
+const createUser = async (path, users, user) => {
+     const isUniqueUser = users.find((u) => u.username === user.username);
+     if (isUniqueUser) {
+          throw new Error('User exists');
+     }
+     users.push(user);
+     writeFile(path, JSON.stringify(users));
+};
+const deleteUserByIndex = async (path, users, index) => {
+     if (index >= users.length) {
+          throw new Error('User not exists');
+     }
+     const deletedUser = users[index];
+     const newUsers = users.filter((_, i) => i !== index);
+     writeFile(path, JSON.stringify(newUsers));
+     return deletedUser;
+};
+const updateUserByIndex = async (path, users, index, updProp) => {
+     if (index >= users.length) {
+          throw new Error('User not exists');
+     }
+     const updUser = users[index];
+     Object.keys(updProp).forEach((key) => {
+          if (key in updUser) {
+               updUser[key] = updProp[key];
+          } else {
+               throw new Error('This property not a found');
+          }
+     });
+     // eslint-disable-next-line no-param-reassign
+     users[index] = updUser;
+     writeFile(path, JSON.stringify(users));
+     return updUser;
+};
 
+const tryParsFile = async (path) => {
+     const users = JSON.parse(await readFile(path));
+     if (!Array.isArray(users)) {
+          const res = await writeFile(path, JSON.stringify([]));
+          return res;
+     }
+     return false;
+};
 
+const validateFile = async (path) => {
+     if (!existsSync(path)) {
+          const res = await writeFile(path, JSON.stringify([]));
+          return res;
+     }
+     try {
+          await tryParsFile(path);
+     } catch (err) {
+          const res = await writeFile(path, JSON.stringify([]));
+          return res;
+     }
+     return false;
+};
 
+const app = express();
+const path = resolve('users.json');
+const port = 3000;
 
+app.use(express.json());
+
+app.get('/users/', async (req, res) => {
+     try {
+          await validateFile(path);
+          const users = JSON.parse(await readFile(path));
+          res.send(JSON.stringify(users));
+     } catch (err) {
+          res.status(500).send(JSON.stringify({ message: err.message }));
+     }
+});
+app.get('/users/:index/', async (req, res) => {
+     try {
+          await validateFile(path);
+          const users = JSON.parse(await readFile(path));
+          const index = +req.params.index;
+          res.send(JSON.stringify(users[index]));
+     } catch (err) {
+          res.status(500).send(JSON.stringify({ message: err.message }));
+     }
+});
+app.post('/users/', async (req, res) => {
+     try {
+          const user = req.body;
+          await checkProperty(user);
+          await checkNames(user.fNname);
+          await checkNames(user.lName);
+          await checkAge(user.age);
+          await validateFile(path);
+          const users = JSON.parse(await readFile(path));
+          await createUser(path, users, req.body);
+          res.status(201).send(JSON.stringify(req.body));
+     } catch (err) {
+          res.status(500).send(JSON.stringify({ message: err.message }));
+     }
+});
+app.deleta('/users/:index/', async (req, res) => {
+     try {
+          await validateFile(path);
+          const users = JSON.parse(await readFile(path));
+          const index = +req.params.index;
+          const deletedUser = await deleteUserByIndex(path, users, index);
+          res.status(200).send(JSON.stringify(deletedUser));
+     } catch (err) {
+          res.status(500).send(JSON.stringify({ message: err.message }));
+     }
+});
+app.patch('/users/:index/', async (req, res) => {
+     try {
+          const user = req.body;
+          await checkProperty(user);
+          await checkNames(user.fNname);
+          await checkNames(user.lName);
+          await checkAge(user.age);
+          await validateFile(path);
+          await validateFile(path);
+          const users = JSON.parse(await readFile(path));
+          const index = +req.params.index;
+          const updUser = await updateUserByIndex(path, users, index, user);
+          res.status(201).send(JSON.stringify(updUser));
+     } catch (err) {
+          res.status(500).send(JSON.stringify({ message: err.message }));
+     }
+});
+
+app.listen(port, () => {
+     // eslint-disable-next-line no-console
+     console.log(`Example app listening on port ${port}!`);
+});
