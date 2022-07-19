7734c70112d55f262db180287c3a77fceefb1fc0
EdgarMihranyan <erichok1000@gmail.com>
2022-07-19 20:44:41 +0400

    Bag update

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -5,6 +5,4 @@ import jwt from 'jsonwebtoken';
 const { JWT_SECRET } = process.env;
 export const sign = (data, expiresIn) => jwt.sign(data, JWT_SECRET, { expiresIn });
 
-export const verify = (token) => {
-     return jwt.verify(token, JWT_SECRET);
-};
+export const verify = (token) => jwt.verify(token, JWT_SECRET);
